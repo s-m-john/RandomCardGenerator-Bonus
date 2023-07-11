187ddbf02e1f4a116faaa9a0fb4884650fe9ab51
s-m-john <107650644+s-m-john@users.noreply.github.com>
2023-07-11 03:00:47 -0400

    Update app.js from original project

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("You see this message every time the page loads!");
+
+  const suites = ["♥", "♠", "♦", "♣"];
+
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  function randomCard(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  let suite = randomCard(suites);
+  console.log("This is the random suite", suite);
+
+  let value = randomCard(cardValues);
+  console.log("This is the random value", value);
+
+  let topsuite = document.querySelector("#topsuite");
+  topsuite.textContent = suite;
+
+  let bottomsuite = document.querySelector("#bottomsuite");
+  bottomsuite.textContent = suite;
+
+  let valueElement = document.querySelector(".number");
+  valueElement.textContent = value;
 };
+
+// console.log("This is the random number", randomCard(suites));
+//};
